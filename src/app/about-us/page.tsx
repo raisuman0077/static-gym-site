84ca56c167033c888ff97efba73e3628f6baa236
raisuman0077 <raisuman0077@gmail.com>
2025-02-13 19:56:08 +0545

    hydration issue fixed

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,17 +5,17 @@ export default function Page() {
         <h1 className="text-6xl font-bold text-white uppercase tracking-wide">
           Who We Are
         </h1>
-        <p className="text-lg text-gray-400 leading-relaxed">
+        <div className="text-lg text-gray-400 leading-relaxed">
           At <span className="font-semibold text-white">IronFit Gym</span>, we empower
           individuals to push their limits and achieve new fitness heights. Our facility
           is designed for athletes of all levels, providing expert guidance, premium
           equipment, and an inspiring community.
-        </p>
+        </div>
       </div>
 
       <div className="w-full max-w-5xl text-center mt-12 space-y-8  ">
         <div className="flex px-4 flex-col md:flex-row justify-between gap-12">
-          <p className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
+          <div className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
             <span className="text-white font-semibold underline">
               🏋️ Expert Trainers:
             </span>
@@ -23,8 +23,8 @@ export default function Page() {
               Get personalized coaching from certified professionals dedicated to your
               success.
             </p>
-          </p>
-          <p className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
+          </div>
+          <div className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
             <span className="text-white font-semibold underline">
               🔥 Advanced Equipment:
             </span>
@@ -32,8 +32,8 @@ export default function Page() {
               Train with cutting-edge machines and free weights tailored to your fitness
               goals.
             </p>
-          </p>
-          <p className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
+          </div>
+          <div className="text-xl text-gray-300 leading-relaxed max-w-md border-2 border-white rounded-xl p-4 text-center">
             <span className="text-white font-semibold flex-wrap underline">
               💪 Strong Community:
             </span>
@@ -41,7 +41,7 @@ export default function Page() {
               Surround yourself with passionate individuals who motivate and inspire every
               day.
             </p>
-          </p>
+          </div>
         </div>
       </div>
     </div>
